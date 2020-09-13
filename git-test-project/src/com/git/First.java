85cb3abc90fb09211242463d46eef269354ac7db
sks <3454981254@qq.com>
2020-09-13 19:20:12 +0800

    update

--- git-test-project/src/com/git/First.java
+++ git-test-project/src/com/git/First.java
@@ -3,9 +3,10 @@ package com.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("gittttttttttttttt");
-
+        First f=new First();
+        f.test();
     }
     public  void test(){
-
+        System.out.println("llll");
     }
 }
