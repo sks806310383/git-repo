11dd6eef2fa9f1aec1fc1c760d7f3bb9cc31c41b
sks <3454981254@qq.com>
2020-09-13 19:05:18 +0800

    本地工程添加到git222

--- git-test-project/src/com/git/First.java
+++ git-test-project/src/com/git/First.java
@@ -3,5 +3,9 @@ package com.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("gittttttttttttttt");
+        this.test();
+    }
+    public void test(){
+        System.out.println("2222222222222222");
     }
 }
